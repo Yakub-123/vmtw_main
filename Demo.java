208f113befa93356146b74cae2bed045e72f304b
Yakub-123 <venkysithub@gmail.com>
2024-06-28 16:25:09 +0530

    my first module successfully completed

---
+++ Demo.java
@@ -0,0 +1,7 @@
+class   Demo
+{
+public static void main(String vmtw[])
+{
+System.out.println("Hello Git");
+}
+}
---
+++ Test.java
@@ -0,0 +1,7 @@
+class   Test
+{
+public static void main(String vmtw[])
+{
+System.out.println("Hello Git");
+}
+}
